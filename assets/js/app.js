717a0a5fce3fb18accd010960a7802fc85539183
Nich Secord <nicholassecord@gmail.com>
2019-06-22 13:59:34 -0700

    gif cards and buttons functional, search not functional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,21 @@
 let topics = [
-    'Option 1',
-    'Option 2',
-    'Option 3',
-    'Option 4',
-    'Option 5',
-    'Option 6',
-    'Option 7',
-    'Option 8',
-    'Option 9',
-    'Option 10',
+    'dogs',
+    'tacos',
+    'snow',
+    'hockey',
+    'cats',
+    'sugar glider',
+    'gallagher',
+    'cricket',
+    'mountains',
+    'stuff',
 ];
+
+$(document).ready(function() {
+    $('#options').on('click', '.btn', function() {
+        getRes(this.innerText);
+    });
+});
 
 function initialButtons(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -17,37 +23,44 @@ function initialButtons(arr) {
     }
 }
 
+function gifCard(url, rating) {
+    const $gifCard = $('<div class="gif-card">');
+    const $gifRating = $('<div class="gif-rating">').text(rating);
+    const $gifImg = $(`<img class='gif-img' src='${url}'>`);
+
+    $gifCard.append($gifRating).append($gifImg);
+
+    $('#gifs').append($gifCard);
+}
+
 function btn(text) {
-    $('#options').append($('<button class="btn">').text(text));
+    const $btn = $(`<button class="btn">`).text(text);
+
+    $('#options').append($btn);
 }
 
 function getRes(req) {
-    const xhr = $.get(
-        `http://api.giphy.com/v1/gifs/search?q=
-        ${slugifyReq(req)}
-        &api_key=vJdz7OAM8teqL32SLGGOQW1KDNmgHztb&limit=10`
-    );
+    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${slugify(
+        req
+    )}&api_key=vJdz7OAM8teqL32SLGGOQW1KDNmgHztb&limit=10`;
 
-    let out = [];
-
-    xhr.done(function(req) {
-        for (let i = 0; i < req.data.length; i++) {
-            out.push(req.data[i].title);
-        }
-        console.log(out);
-        return out;
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(res) {
+        $.each(res.data, (i, gif) => {
+            gifCard(gif.images['downsized'].url, gif.rating);
+        });
     });
 }
 
-function slugifyReq(req) {
+function search(req) {
+    btn(req);
+    getRes(req);
+}
+
+function slugify(req) {
     return req.replace(/\s+/g, '+').toLowerCase();
 }
 
-function addGifs(req) {
-    const res = getRes(req);
-
-    console.log(res);
-}
 initialButtons(topics);
-
-addGifs('hi');
